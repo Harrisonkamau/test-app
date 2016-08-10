6ae6a584c571dac26306d260953db557af274c78
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-10 17:45:48 +0300

    create a user model

--- models.py
+++ models.py
@@ -10,11 +10,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), unique=True)
     last_name = db.Column(db.String(80), unique=True)
-    date_of_birth = db.Column(Date, unique=True)
+    # date_of_birth = db.Column(db.Date, unique=True)
 
-    def __init__(self, first_name, last_name, date_of_birth):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-        self.date_of_birth = date_of_birth
+        # self.date_of_birth = date_of_birth
 
 
