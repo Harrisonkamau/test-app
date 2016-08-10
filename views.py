30594ade1f5a76955f990f8b7796e6fd2b8d8e15
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-10 17:46:44 +0300

    render home page

--- views.py
+++ views.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # create a home route
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('index.html')
 # start the server
 if __name__ == "__main__":
     app.run(debug=True)
